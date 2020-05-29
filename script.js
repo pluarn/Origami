5f3bcb656da27d35343aebc1a9b9305b8cc0ec98
pluarn <pluarn@uw.edu>
2020-05-29 12:21:05 -0700

    fixed pics on turtle and crane page, added google translate on all pages

--- script.js
+++ script.js
@@ -28,3 +28,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
